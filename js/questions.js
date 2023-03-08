2bd3bf706c3a561b58f61c48b61f6df818e1e99c
NithinBairoju <120241069+NithinBairoju@users.noreply.github.com>
2023-03-08 20:22:11 +0530

    touch event added

--- js/questions.js
+++ js/questions.js
@@ -5,31 +5,23 @@ bar.forEach((e) => {
     e.addEventListener('mouseover', () => {
         open(e);
     })
+     e.addEventListener('touchstart', () => {
+        open(e);
+    })
 })
 
 
 function open(elm) {
     let content = elm.nextElementSibling;
     content.classList.add('p-active');
+    
     // let content2 = elm.firstChild.nextElementSibling;
     // let content3 = content2.nextElementSibling;
-
-    // if (content3.style.transform === "rotate(44deg)") {
-    //     content3.style.transform = "rotate(0)";
-    // } else {
-    //     content3.style.transform = "rotate(44deg)";
-    // }
-    // console.log(content3);
+    
     elm.addEventListener('mouseout', () => {
         let content = elm.nextElementSibling;
         content.classList.remove('p-active');
-        // let content2 = elm.firstChild.nextElementSibling;
-        // let content3 = content2.nextElementSibling;
-        // if (content3.style.transform === "rotate(44deg)") {
-        //     content3.style.transform = "rotate(0)";
-        // } else {
-        //     content3.style.transform = "rotate(44deg)";
-        // }
+     
     })
 
 }
