a01f26dbc0878f4b3f1a359b0a7a912c50e4e67a
NithinBairoju <120241069+NithinBairoju@users.noreply.github.com>
2023-03-08 20:56:25 +0530

    update 10

--- js/questions.js
+++ js/questions.js
@@ -2,26 +2,35 @@ const bar = document.querySelectorAll('.collapse');
 const para = document.querySelectorAll('.p-none');
 let valueImg = false;
 bar.forEach((e) => {
-
-        e.addEventListener('click', () => {
-            open(e);
-        })
-    
+    e.addEventListener('mouseover', () => {
+        open(e);
+    })
 })
 
 
 function open(elm) {
     let content = elm.nextElementSibling;
     content.classList.add('p-active');
-    
     // let content2 = elm.firstChild.nextElementSibling;
     // let content3 = content2.nextElementSibling;
 
-        elm.addEventListener('click', () => {
-            let content = elm.nextElementSibling;
-            content.classList.remove('p-active');
-         })
-
+    // if (content3.style.transform === "rotate(44deg)") {
+    //     content3.style.transform = "rotate(0)";
+    // } else {
+    //     content3.style.transform = "rotate(44deg)";
+    // }
+    // console.log(content3);
+    elm.addEventListener('mouseout', () => {
+        let content = elm.nextElementSibling;
+        content.classList.remove('p-active');
+        // let content2 = elm.firstChild.nextElementSibling;
+        // let content3 = content2.nextElementSibling;
+        // if (content3.style.transform === "rotate(44deg)") {
+        //     content3.style.transform = "rotate(0)";
+        // } else {
+        //     content3.style.transform = "rotate(44deg)";
+        // }
+    })
 
 }
 
